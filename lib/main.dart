f60225d6fbdef4c515360119586ce54da0566d5b
wecodelife_intern <wecodelifepvtltd@gmail.com>
2024-01-23 16:24:19 +0530

    test

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
+    print("jhgj");
   );
   runApp(MyApp());
 }
